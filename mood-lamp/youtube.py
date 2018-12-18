c95afd97262e9e35e5a85366f1b61f994f1b3b78
김대호 <eogh2811@naver.com>
2018-12-18 17:51:31 +0900

    Update youtube.py
    다운로드 큐 클래스 추가

--- mood-lamp/youtube.py
+++ mood-lamp/youtube.py
@@ -1,8 +1,10 @@
 import youtube_dl
+import threading
 
-class WaitingItem:
+class Waiting_Item:
     def __init__(self, url):
         self.url = url
+        self.name = None
         self.options = {
             "format" : "bestaudio/best",
             "extractaudio" : True,
@@ -33,9 +35,62 @@ class WaitingItem:
             with youtube_dl.YoutubeDL(self.options) as ytdl:
                 fileInfo = ytdl.extract_info(self.url, download = False)
                 ytdl.download([self.url])
+            self.name = fileInfo["title"]
             return True
         except:
             return False
+
+class Download_Queue(list):
+    def enqueue(self, item: Waiting_Item):
+        self.append(item)
+
+    def dequeue(self):
+        return self.pop(0)
+
+    def is_empty(self):
+        return not len(self) > 0
+
+    def play(self):
+        if self.is_empty():
+            print("대기열에 재생할 곡이 없습니다.")
+            return
+        else:
+            item = self.dequeue()
+            # 물품 도착 후 세부 구현 예정
+            if item.download():
+                print("%s 재생을 시작합니다." % item.name)
+            else:
+                print("%s 다운로드에 실패했습니다." % item.name)
+            # 물품 도착 후 세부 구현 예정
+
+def find_video(keyword: str) -> str:
+    """유튜브에서 찾고 싶은 키워드를 입력받고, 그 동영상의 URL를 돌려줍니다.
+
+    매개 변수
+    ----------
+    keyword: str
+        찾고 싶은 키워드
+
+    리턴
+    -------
+    str
+        검색 완료된 영상의 URL
+    """
+    options = {
+        "format" : "bestaudio/best",
+        "extractaudio" : True,
+        "audioformat" : "mp3",
+        "outtmpl" : "%(title)s.%(ext)s",
+        "noplaylist" : True,
+        "nocheckcertificate" : True,
+        "ignoreerrors" : True,
+        "logtostderr" : False,
+        "quiet" : True,
+        "no_warnings" : True
+    }
+
+    with youtube_dl.YoutubeDL(options) as ytdl:
+        return ytdl.extract_info("ytsearch1:%s" % keyword, download = False)["entries"][0]["webpage_url"]
 
 if __name__ == "__main__":
     pass
