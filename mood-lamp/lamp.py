5e27114578c8b3fc060137f6586f0d447b4f2901
GuGU <wnduddl3409@naver.com>
2018-12-20 19:46:03 +0900

    Update lamp.py
    오류 제거

--- mood-lamp/lamp.py
+++ mood-lamp/lamp.py
@@ -1,4 +1,4 @@
-# 라즈베리에서 실행
+# 주석 친 코드는 라즈베리에서 실행
 
 import gsm_weather
 # import Adafruit_CharLCD
@@ -6,28 +6,18 @@ import gsm_weather
 
 # 날씨정보에서 카테고리에 관한 결과 코드 전달
 def get_result_code(ctgory : str) -> float:
-    key = "Bo6Lak%2BejKb2tdv6MzSBk3GSG8DD5CFO6Fkcsiq%2BQFtK5%2BaSETARXl%2F7mBCpaWCN98MNTPh4DoZDeV1plXWc9A%3D%3D"
-    weather_dict = gsm_weather.get_weather_info(key)["response"]["body"]["items"]["item"]
-    
-    for i in weather_dict:
+    # key = ""
+    weather_list = gsm_weather.get_weather_info(key)["response"]["body"]["items"]["item"]
+
+    for i in weather_list:
         if i["category"] == ctgory:
             return i["fcstValue"]
-
-'''
-def sky_str(sky):
-    status_list = ["Sunny", "Cloudy", "Cloudy", "Blur"]
-    return status_list[sky - 1]
-
-def pty_str(pty):
-    status_list = ["None", "Rainy", "Sleet", "Snowy"]
-    return status_list[pty]
-'''
 
 def inform_sky_pty(sky, pty):
     skylist = ["Sunny", "Cloudy", "Cloudy", "Blur"]
     ptylist = ["None", "Rainy", "Sleet", "Snowy"]
     
-    if pty != 0: # 강수형태가 없음(0)이 아니면
+    if not pty: # 강수형태가 없음(0)이 아니면
         return skylist[sky - 1]
     return ptylist[pty]
 
@@ -36,16 +26,20 @@ def inform_sky_pty(sky, pty):
 # 강수형태(PTY) 코드 : 없음(0), 비(1), 진눈깨비(2), 눈(3)
 
 # 이 파일을 직접 실행해야 작동되는 부분
+
+
 if __name__ == "__main__":
     # lcd = Adafruit_CharLCD(rs = 22, en = 11, d4 = 23, d5 = 10, d6 = 9, d7 = 25, cols = 16, lines = 2)
 
     # sky : 하늘상태, pty : 강수형태
+    
     sky, pty = get_result_code("SKY"), get_result_code("PTY")
     t3h = get_result_code("T3H")
-
+    
     weather_status = inform_sky_pty(sky, pty)
-
+    
     # print(sky, pty, t3h, weather_status)
 
     # lcd.message("Weather : " + weather_status + "\n") # 날씨 상태
     # lcd.message("Temperature : " + t3h) # 기온
+ 
