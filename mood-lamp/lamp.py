cb0d8342a7bb3f7a1918051a8e2b5c4af483d8e2
GuGU <wnduddl3409@naver.com>
2018-12-20 18:09:18 +0900

    lamp.py
    sky와 pty 합친 함수 추가
    온도 추가

--- mood-lamp/lamp.py
+++ mood-lamp/lamp.py
@@ -2,7 +2,7 @@
 
 import gsm_weather
 import Adafruit_CharLCD
-import RPI.GPIO
+import RPI.GPIO as GPIO
 
 # 날씨정보에서 카테고리에 관한 결과 코드 전달
 def get_result_code(ctgory : str) -> str:
@@ -12,34 +12,37 @@ def get_result_code(ctgory : str) -> str:
         if i["category"] == ctgory:
             return i["fcstValue"]
 
+'''
 def sky_str(sky):
-    if sky == 1:
-        return "Sunny" # 맑음
-    elif sky == 2:
-        return "Cloudy" # 구름조금
-    elif sky == 3:
-        return "Cloud" # 구름많이
-    else:
-        return "Blur" # 흐림
+    status_list = ["Sunny", "Cloudy", "Cloudy", "Blur"]
+    return status_list[sky - 1]
 
 def pty_str(pty):
-    if pty == 0:
-        return "None" # 없음
-    elif pty == 1:
-        return "Rainy" # 비
-    elif pty == 2:
-        return "Sleet" # 진눈깨비
-    else:
-        return "Snowy" # 눈
+    status_list = ["None", "Rainy", "Sleet", "Snowy"]
+    return status_list[pty]
+'''
 
+def inform_sky_pty(sky, pty):
+    skylist = ["Sunny", "Cloudy", "Cloudy", "Blur"]
+    ptylist = ["None", "Rainy", "Sleet", "Snowy"]
+    
+    if pty != 0: # 강수형태가 없음(0)이 아니면
+        return skylist[sky - 1]
+    return ptylist[pty]
+
+
+# 하늘상태(SKY) 코드 : 맑음(1), 구름낀(2, 3), 흐림(4)
+# 강수형태(PTY) 코드 : 없음(0), 비(1), 진눈깨비(2), 눈(3)
+
+# 이 파일을 직접 실행해야 작동되는 부분
 if __name__ == "__main__":
     lcd = Adafruit_CharLCD(rs = 22, en = 11, d4 = 23, d5 = 10, d6 = 9, d7 = 25, cols = 16, lines = 2)
 
-    # sky : 날씨상태, pty : 강수상태
+    # sky : 하늘상태, pty : 강수형태
     sky, pty = get_result_code("SKY"), get_result_code("PTY")
+    t3h = get_result_code("T3H")
 
-    sky = sky_str(sky)
-    pty = pty_str(pty)
+    weather_status = inform_sky_pty(sky, pty)
 
-    lcd.message("Weather : " + sky + "\n") # 날씨상태
-    lcd.message("PPTN type : " + pty) # 강수형태
+    lcd.message("Weather : " + weather_status + "\n") # 날씨 상태
+    lcd.message("Temperature : " + t3h) # 기온
