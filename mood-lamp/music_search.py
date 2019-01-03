42236665c7ad99b2c932e67a30e721c718865ec3
김대호 <eogh2811@naver.com>
2019-01-03 16:49:38 +0900

    Update music_search.py
    함수 설명을 붙여놓음

--- mood-lamp/music_search.py
+++ mood-lamp/music_search.py
@@ -1,29 +1,32 @@
 import requests
 from bs4 import BeautifulSoup
 
-
-
-
-global header
-header = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Trident/7.0; rv:11.0) like Gecko'}
-
+def get_html(url):
+    header = {"User-Agent": "Mozilla/5.0 (Windows NT 6.3; Trident/7.0; rv:11.0) like Gecko"}
+    return requests.get(url, headers = header).text
 
 def search_music(music):
-    try :
-        melon = requests.get('https://www.melon.com/search/total/index.htm?q={0}&section=&linkOrText=T&ipath=srch_form'.format(music), headers = header)
-        melon_html = melon.text
-        melon_parse = BeautifulSoup(melon_html, 'html.parser')
-        title = melon_parse.select('#frm_searchSong > div > table > tbody > tr:nth-of-type(1) > td:nth-of-type(3) > div > div > a.fc_gray')
-        artist = melon_parse.select('#artistName > a')
-        return artist[0].text,title[0].text
+    """Melon 음악 사이트에서 검색을 해서 실존하는지 검사합니다.
+
+    매개 변수
+    ----------
+    music: str
+        검색을 원하는 검색어
+
+    리턴
+    -------
+    tuple
+        (노래 제목, 노래를 부른 가수)로 구성된 튜플, 검색 결과가 없다면 None을 반환한다.
+    """
+    try:
+        melon_html = get_html("https://www.melon.com/search/total/index.htm?q=%s&section=&linkOrText=T&ipath=srch_form" % music)
+        melon_parse = BeautifulSoup(melon_html, "html.parser")
+        
+        title = melon_parse.select("#frm_searchSong > div > table > tbody > tr:nth-of-type(1) > td:nth-of-type(3) > div > div > a.fc_gray")
+        artist = melon_parse.select("#artistName > a")
+        return (artist[0].text, title[0].text)
     except IndexError:
-        return "노래가 없습니다"
+        return (None, None)
 
 if __name__ == "__main__":
-    while 1:
-        msg = input("입력 : ")
-        print(search_music(msg))
-
-
-    
-#frm_searchSong > div > table > tbody > tr:nth-child(1) > td:nth-child(3) > div > div > a.fc_gray
+    pass
