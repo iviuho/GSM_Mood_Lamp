9afec0ab27522b097a07f76ea5e5714ced2052a1
박준건 <pjg3239@naver.com>
2019-01-07 16:52:14 +0900

    Update command.py
    틀어 줘도 이제 할 수 있습니다ㅎㅎ!!

--- mood-lamp/command.py
+++ mood-lamp/command.py
@@ -2,7 +2,7 @@ class Command_Manager:
     def __init__(self, path: str):
         try:
             with open(path, "r", encoding = "UTF8") as f:
-                self.command_list = f.read().split() # word_list는 "틀어줘, 켜, 틀어주세요"라는 단어가 들어감
+                self.command_list = f.read().split('\n') # word_list는 "틀어줘, 켜, 틀어주세요"라는 단어가 들어감
         except FileNotFoundError:
             print(path, "파일이 없음")
 
@@ -19,10 +19,13 @@ class Command_Manager:
         bool
             포함된다면(명령어가 맞다면) True, 아니면 False
         """
-        return True if text.split()[-1] in self.command_list else False
+        if text.split()[-1] in self.command_list:
+            return True
+        else:
+            return True if " ".join(text.split()[-2:]) in self.command_list else False
 
 if __name__ == "__main__":
-    manager = Command_Manager("word.txt")
+    manager = Command_Manager("command.txt")
     while True:
         msg = input("입력 : ")
         print(manager.is_command(msg))
